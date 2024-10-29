9da832580782b64d4b5248920bb75cfd4f033ec5
DemondLive <anaislamaslinda993@gmail.com>
2024-10-29 15:53:04 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -6,7 +6,7 @@ st.sidebar.title("my first barra lateral")
 st.sidebar.header("holis barra lateral")
 st.sidebar.write("esto es una barra lateral")
 
-st.sidebar.image("_93356478_trumpyobama1epanov10.jpg")
+st.sidebar.image("ESdyfEIXcAEArWx.jpg")
 
 if st.sidebar.button("haz clik in the barra lateral"):
     st.sidebar.write("clik hecho")
